bf6fbe90075d9fa40d8630950a35cc371df3fda0
Rotadeamon <73108975+EnricoSch-Github@users.noreply.github.com>
2023-02-14 20:22:21 +0100

    Update FileAndMotdGenerator.py

--- FileAndMotdGenerator.py
+++ FileAndMotdGenerator.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import shutil
+import random
 
 Modpackname = input('Enter the Modpack name:')
 
@@ -15,13 +16,24 @@ if(os.path.exists("C:\MotdList\ " + dir_name)):
 
 os.makedirs("C:\MotdList\ " + dir_name)
 
+# Creating here an Arry with all Modpacknames / and create BString with 3 random Modpacks with the Motd Prefix and without duplication of the modpacks name
+
+ModpackNames = ["Legend of the Eyes", "Better MC", "Encrpted", "Sky Vault Hunters", "Stoneblock 3", "RAD2", "Odyssey:Space - A New Beginning", "Tekkit 2", "FTB Interactions", 
+"Nomifactory (GTCEu Port)", "Technological Journey", "Enigmatica 2:Expert - Extended", "MeatballCraft", "Divine Journey 2", "Project Ozone 3", "FTB Skyfactory 3", 
+"GT New Horizons", "TerraFirmaPunk", "Regrowth"]
+
+# 19 Modpacks
+RandomPacks = random.sample(ModpackNames, 3)
+String =" &7&l&e ".join(map(str,RandomPacks))
+ModpackNames3 = " &7&l&e " + String
+
+
 # Creating here a .txt file where the motd code is inside
 
-radio = "{radio}"
 
 File = open('C:\MotdList\ 'f"{Modpackname}\custommotdlist.txt",'w+')
-File.write("\n &7&l//- &6&l" + f"{Modpackname} &r&7&l-\\|&7{radio}&l&e ATM7S &7- &l&eNF &7- &l&eNFu &7- &l&eTekkit 2 &7{radio}")
-File.write("\n &7&l//- &6&l" + f"{Modpackname} &r&7&l-\\|&7{radio}&l&e CU &7- &l&eCC &7- &l&eEE2E &7- &l&eDH - &l&eIEE - &l&eKP3 &7{radio}")
+File.write("\n &7&l//- &6&l" + f"{Modpackname} &r&7&l-\\| {ModpackNames3}")
+File.write("\n &7&l//- &6&l" + f"{Modpackname} &r&7&l-\\| {ModpackNames3}")
 File.close()
 
 File = open('C:\MotdList\ 'f"{Modpackname}\customplayerlist.txt",'w+')
@@ -29,8 +41,11 @@ File.write("\n &6&k{radio}&r&f&l Valhalla MC Network &r&6&k{radio}&r")
 File.write("\n &7==========================&r")
 File.write("\n &7- &6&oDoes have discord integration :D")
 File.write("\n &7- &9&oAlways chunkloaded (offline)")
+File.write("\n &7- Free Cakes from Alp :3")
 File.write("\n ")
 File.write("\n &7==========================&r")
 File.write("\n &l&oPlayers Online: {playercount}/{maxplayers}")
 File.write("\n Difficulty: {difficulty}")
 File.write("\n &8Minecraft Version: &8&l{mcversion}")
+File.close()
+
