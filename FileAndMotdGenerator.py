2092941c7940abd26a7a990cfdc5ce3223a3f2f0
Rotadeamon <73108975+EnricoSch-Github@users.noreply.github.com>
2023-03-19 15:37:51 +0100

    Add files via upload
    Added new modpacks and delete old ones

--- FileAndMotdGenerator.py
+++ FileAndMotdGenerator.py
@@ -18,22 +18,19 @@ os.makedirs("C:\MotdList\ " + dir_name)
 
 # Creating here an Arry with all Modpacknames / and create BString with 3 random Modpacks with the Motd Prefix and without duplication of the modpacks name
 
-ModpackNames = ["Legend of the Eyes", "Better MC", "Encrpted", "Sky Vault Hunters", "Stoneblock 3", "RAD2", "Odyssey:Space - A New Beginning", "Tekkit 2", "FTB Interactions", 
-"Nomifactory (GTCEu Port)", "Technological Journey", "Enigmatica 2:Expert - Extended", "MeatballCraft", "Divine Journey 2", "Project Ozone 3", "FTB Skyfactory 3", 
-"GT New Horizons", "TerraFirmaPunk", "Regrowth"]
+ModpackNames = ["Nomifactory", "TJ", "EE2E", "GTNH", "FTB Interactions", "SB3", "MBC", "Better MC", "Regrowth", "Po3", "GRRED" , "Decursio", "The Lost Era", "E6E", "Astral Space Reborn", "Ultimate Progression", "VH 3rd Edition"]
 
 # 19 Modpacks
 def CreateModpackNames():
     RandomPacks = random.sample(ModpackNames, 3)
-    String =" &7&l&e ".join(map(str,RandomPacks))
+    String =" &7&l&e, ".join(map(str,RandomPacks))
     return " &7&l&e " + String
-
 
 # Creating here a .txt file where the motd code is inside
 
 File = open('C:\MotdList\ 'f"{Modpackname}\custommotdlist.txt",'w+')
 for x in range(20):
-    File.write("\n &7&l//- &6&l" + f"{Modpackname} &r&7&l-\\|"+ CreateModpackNames())
+    File.write("\n &7&l//- &6&l" + f"{Modpackname} &r&7&l-\\\|"+ CreateModpackNames())
 File.close()
 
 File = open('C:\MotdList\ 'f"{Modpackname}\customplayerlist.txt",'w+')
@@ -41,7 +38,7 @@ File.write("\n &6&k{radio}&r&f&l Valhalla MC Network &r&6&k{radio}&r")
 File.write("\n &7==========================&r")
 File.write("\n &7- &6&oDoes have discord integration :D")
 File.write("\n &7- &9&oAlways chunkloaded (offline)")
-File.write("\n &7- Free Cakes from Alp :3")
+File.write("\n &7- Free Cakes ")
 File.write("\n ")
 File.write("\n &7==========================&r")
 File.write("\n &l&oPlayers Online: {playercount}/{maxplayers}")
